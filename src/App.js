877d5d34489b44b58a0dd58f31effb5500a64c6e
EmilyWatkinz <sewatkins16@gmail.com>
2026-01-30 10:25:55 -0800

    Skinstric Landing Page - no animations yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-left-text">
+        <span className="skinstric-text">Skinstric</span>
+        <span className="intro-text">[ Intro ]</span>
+      </div>
+      <img src="/header.png" alt="Header" className="header-image" />
+      <div className="triangle triangle-left"></div>
+      <div className="triangle triangle-right"></div>
+      <img src="/ai-button-icon-text-shrunk.png" alt="AI Button" className="ai-button" />
+      <img src="/button-icon-text-shrunk.png" alt="Take Test" className="take-test-button" />
+      <div className="sophisticated-page">
+        <div className="sophisticated-text">Sophisticated</div>
+        <div className="skincare-text">skincare</div>
+      </div>
+      <div className="bottom-left-text">
+        Skinstric developed an A.I. that creates<br />
+        a highly-personalised routine tailored<br />
+        to what your skin needs.
+      </div>
     </div>
   );
 }
